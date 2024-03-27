0b3e0d1d3e29ed0affd455e3ec91b64b90c61c18
jerrylin <jerrylin920316@gmail.com>
2024-03-27 17:11:20 +0800

    刪除 - django 專案

--- smart_operation_digital_dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('products/', include('dashboard.urls')),
-    path('accounts/', include('allauth.urls')),
-    path('admin/', admin.site.urls),
-]
