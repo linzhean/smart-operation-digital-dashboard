78da961d6be55da5550a6b38c14d6ee3a1036a44
jerrylin <jerrylin920316@gmail.com>
2024-04-02 14:30:36 +0800

    新增 - 權限 Enum

--- src/main/java/tw/edu/ntub/imd/birc/sodd/bean/UserAccountBean.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/bean/UserAccountBean.java
@@ -1,4 +1,23 @@
 package tw.edu.ntub.imd.birc.sodd.bean;
 
+import lombok.Data;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.enumerate.Identity;
+
+import java.time.LocalDateTime;
+
+@Data
 public class UserAccountBean {
+    private String userId;
+    private String userName;
+    private String departmentId;
+    private String googleId;
+    private String gmail;
+    private Identity identity;
+    private String position;
+    private Boolean available;
+    private String createId;
+    private LocalDateTime createDate;
+    private String modifyId;
+    private LocalDateTime modifyDate;
+
 }
