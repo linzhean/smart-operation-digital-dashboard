60619f022dff3fc046e803b20533a663604a0721
jerrylin <jerrylin920316@gmail.com>
2024-04-10 15:48:45 +0800

    新增 - 部門相關檔案

--- src/main/java/tw/edu/ntub/imd/birc/sodd/service/DepartmentService.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/service/DepartmentService.java
@@ -1,2 +1,10 @@
-package tw.edu.ntub.imd.birc.sodd.service;public interface DepartmentService {
+package tw.edu.ntub.imd.birc.sodd.service;
+
+import tw.edu.ntub.imd.birc.sodd.bean.DepartmentBean;
+
+import java.util.Map;
+
+
+public interface DepartmentService extends BaseService<DepartmentBean, Integer> {
+    Map<String, String> getDepartmentMap();
 }
