f25c46648aed310d64a3930b1c4782b9d3a4e5f5
jerrylin <jerrylin920316@gmail.com>
2024-04-26 13:57:15 +0800

    修改 - 加入Vaildation驗證

--- src/main/java/tw/edu/ntub/imd/birc/sodd/bean/DashboardBean.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/bean/DashboardBean.java
@@ -2,15 +2,26 @@ package tw.edu.ntub.imd.birc.sodd.bean;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 public class DashboardBean {
+    @Null(message = "儀表板流水號 - 不得填寫")
     private Integer id;
+    @Size(max = 254, message = "儀表板名稱 - 不得超過{max}個字")
+    @NotNull(message = "儀表板名稱 - 未填寫")
     private String name;
+    @Null(message = "是否可用 - 不得填寫")
     private Boolean available;
+    @Null(message = "創建人ID - 不得填寫")
     private String createId;
+    @Null(message = "創建日期 - 不得填寫")
     private LocalDateTime createDate;
+    @Null(message = "修改人ID - 不得填寫")
     private String modifyId;
+    @Null(message = "修改日期 - 不得填寫")
     private LocalDateTime modifyDate;
 }
--- src/main/java/tw/edu/ntub/imd/birc/sodd/bean/UserAccountBean.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/bean/UserAccountBean.java
@@ -3,13 +3,22 @@ package tw.edu.ntub.imd.birc.sodd.bean;
 import lombok.Data;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.enumerate.Identity;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
 public class UserAccountBean {
+    @Size(max = 254, message = "email - 不可超過{max}個字")
+    @NotBlank(message = "email - 未填寫")
     private String userId;
+    @Size(max = 45, message = "姓名 - 不可超過{max}個字")
+    @NotBlank(message = "姓名 - 未填寫")
     private String userName;
     private String departmentId;
+    @Null(message = "google Id - 不得事先填寫")
     private String googleId;
     private String gmail;
     private Identity identity;
