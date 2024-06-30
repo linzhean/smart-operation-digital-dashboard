3568db357601a3e30341bbfaf888d74cff144536
jerrylin <jerrylin920316@gmail.com>
2024-06-30 14:26:57 +0800

    修改 - 交辦功能，交辦權限管理

--- modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/dao/AssignedTaskSponsorDAO.java
+++ modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/dao/AssignedTaskSponsorDAO.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Repository;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.AssignedTaskSponsor;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.AssignedTaskSponsorId;
 
+import java.util.List;
+
 @Repository
 public interface AssignedTaskSponsorDAO extends BaseDAO<AssignedTaskSponsor, AssignedTaskSponsorId> {
+    List<AssignedTaskSponsor> findBySponsorUserIdAndAvailableIsTrue(String userId);
+
+    List<AssignedTaskSponsor> findByChartIdAndAvailableIsTrue(Integer chartId);
 }
--- src/main/java/tw/edu/ntub/imd/birc/sodd/service/impl/AssignedTaskSponsorServiceImpl.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/service/impl/AssignedTaskSponsorServiceImpl.java
@@ -1,12 +1,15 @@
 package tw.edu.ntub.imd.birc.sodd.service.impl;
 
 import org.springframework.stereotype.Service;
+import tw.edu.ntub.birc.common.util.CollectionUtils;
 import tw.edu.ntub.imd.birc.sodd.bean.AssignedTaskSponsorBean;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.dao.AssignedTaskSponsorDAO;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.AssignedTaskSponsor;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.AssignedTaskSponsorId;
 import tw.edu.ntub.imd.birc.sodd.service.AssignedTaskSponsorService;
 import tw.edu.ntub.imd.birc.sodd.service.transformer.AssignedTaskSponsorTransformer;
+
+import java.util.List;
 
 @Service
 public class AssignedTaskSponsorServiceImpl extends BaseServiceImpl<AssignedTaskSponsorBean, AssignedTaskSponsor, AssignedTaskSponsorId>
@@ -23,5 +26,35 @@ public class AssignedTaskSponsorServiceImpl extends BaseServiceImpl<AssignedTask
     @Override
     public AssignedTaskSponsorBean save(AssignedTaskSponsorBean assignedTaskSponsorBean) {
         return null;
+    }
+
+    @Override
+    public List<AssignedTaskSponsorBean> findByUserId(String userId) {
+        return CollectionUtils.map(
+                assignedTaskSponsorDAO.findBySponsorUserIdAndAvailableIsTrue(userId), transformer::transferToBean);
+    }
+
+    @Override
+    public List<AssignedTaskSponsorBean> finByChartId(Integer chartId) {
+        return CollectionUtils.map(
+                assignedTaskSponsorDAO.findByChartIdAndAvailableIsTrue(chartId), transformer::transferToBean);
+    }
+
+    @Override
+    public AssignedTaskSponsorBean save(Integer chartId, String userId) {
+        AssignedTaskSponsor sponsor = new AssignedTaskSponsor();
+        sponsor.setChartId(chartId);
+        sponsor.setSponsorUserId(userId);
+        return transformer.transferToBean(assignedTaskSponsorDAO.save(sponsor));
+    }
+
+    @Override
+    public void removeSponsorFromChart(String userId, Integer chartId) {
+        AssignedTaskSponsorBean sponsorBean = new AssignedTaskSponsorBean();
+        sponsorBean.setAvailable(false);
+        AssignedTaskSponsorId sponsorId = new AssignedTaskSponsorId();
+        sponsorId.setSponsorUserId(userId);
+        sponsorId.setChartId(chartId);
+        update(sponsorId, sponsorBean);
     }
 }
