37cca02e3cfc08f851ab0b9ced20278ad479a25b
jerrylin <jerrylin920316@gmail.com>
2024-06-30 14:14:19 +0800

    新增 - 郵件與訊息功能

---
+++ modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/entity/listener/MailMessageListener.java
@@ -0,0 +1,43 @@
+package tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.listener;
+
+import tw.edu.ntub.birc.common.util.StringUtils;
+import tw.edu.ntub.imd.birc.sodd.config.util.SecurityUtils;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.Mail;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.MailMessage;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+public class MailMessageListener {
+    @PrePersist
+    public void preSave(MailMessage mailMessage) {
+        String userId = SecurityUtils.getLoginUserAccount();
+        LocalDateTime now = LocalDateTime.now();
+        if (mailMessage.getAvailable() == null) {
+            mailMessage.setAvailable(true);
+        }
+        if (StringUtils.isBlank(userId)) {
+            mailMessage.setCreateId(userId);
+        }
+        if (mailMessage.getCreateDate() == null) {
+            mailMessage.setCreateDate(now);
+        }
+        if (StringUtils.isBlank(mailMessage.getModifyId())) {
+            mailMessage.setModifyId(userId);
+        }
+        if (mailMessage.getModifyDate() == null) {
+            mailMessage.setModifyDate(now);
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(MailMessage mailMessage) {
+        if (StringUtils.isBlank(mailMessage.getModifyId())) {
+            mailMessage.setModifyId(SecurityUtils.getLoginUserAccount());
+        }
+        if (mailMessage.getModifyDate() == null) {
+            mailMessage.setModifyDate(LocalDateTime.now());
+        }
+    }
+}
