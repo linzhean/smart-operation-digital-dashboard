835781ffaec801ecec0fe0098ce4b4339138ca3a
jerrylin <jerrylin920316@gmail.com>
2024-07-03 17:44:49 +0800

    新增 - 使用者列表條件查詢功能

--- modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/dao/UserAccountDAO.java
+++ modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/dao/UserAccountDAO.java
@@ -1,8 +1,10 @@
 package tw.edu.ntub.imd.birc.sodd.databaseconfig.dao;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.UserAccount;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.UserAccount_;
 
 @Repository
-public interface UserAccountDAO extends BaseDAO<UserAccount, String> {
+public interface UserAccountDAO extends BaseDAO<UserAccount, String>, JpaSpecificationExecutor<UserAccount> {
 }
