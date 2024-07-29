1645757797587872c12260dcef37c374c84b49b5
jerrylin <jerrylin920316@gmail.com>
2024-07-29 21:57:20 +0800

    新增 - 群組設定可視圖表功能

--- src/main/java/tw/edu/ntub/imd/birc/sodd/controller/GroupController.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/controller/GroupController.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import tw.edu.ntub.imd.birc.sodd.bean.ChartGroupBean;
 import tw.edu.ntub.imd.birc.sodd.bean.GroupBean;
 import tw.edu.ntub.imd.birc.sodd.bean.UserAccountBean;
 import tw.edu.ntub.imd.birc.sodd.bean.UserGroupBean;
+import tw.edu.ntub.imd.birc.sodd.service.ChartGroupService;
 import tw.edu.ntub.imd.birc.sodd.service.DepartmentService;
 import tw.edu.ntub.imd.birc.sodd.service.GroupService;
 import tw.edu.ntub.imd.birc.sodd.service.UserGroupService;
@@ -15,16 +17,21 @@ import tw.edu.ntub.imd.birc.sodd.util.http.ResponseEntityBuilder;
 import tw.edu.ntub.imd.birc.sodd.util.json.array.ArrayData;
 import tw.edu.ntub.imd.birc.sodd.util.json.object.ObjectData;
 
+import javax.servlet.http.HttpServletRequest;
+
 @AllArgsConstructor
 @RestController
 @RequestMapping(path = "/group")
 public class GroupController {
     private final GroupService groupService;
     private final UserGroupService userGroupService;
+    private final ChartGroupService chartGroupService;
     private final DepartmentService departmentService;
 
     @PostMapping("")
-    public ResponseEntity<String> addGroup(@RequestBody GroupBean groupBean, BindingResult bindingResult) {
+    public ResponseEntity<String> addGroup(@RequestBody GroupBean groupBean,
+                                           BindingResult bindingResult,
+                                           HttpServletRequest request) {
         BindingResultUtils.validate(bindingResult);
         groupService.save(groupBean);
         return ResponseEntityBuilder.success()
@@ -34,7 +41,8 @@ public class GroupController {
 
     @PostMapping("/user")
     public ResponseEntity<String> addUserToGroup(@RequestParam("userId") String userId,
-                                                 @RequestParam("groupId") Integer groupId) {
+                                                 @RequestParam("groupId") Integer groupId,
+                                                 HttpServletRequest request) {
         UserGroupBean userGroupBean = new UserGroupBean();
         userGroupBean.setUserId(userId);
         userGroupBean.setGroupId(groupId);
@@ -44,8 +52,21 @@ public class GroupController {
                 .build();
     }
 
+    @PostMapping("/chart")
+    public ResponseEntity<String> addChartToGroup(@RequestParam("groupId") Integer groupId,
+                                                 @RequestParam("chartId") Integer chartId,
+                                                 HttpServletRequest request) {
+        ChartGroupBean chartGroupBean = new ChartGroupBean();
+        chartGroupBean.setGroupId(groupId);
+        chartGroupBean.setChartId(chartId);
+        chartGroupService.save(chartGroupBean);
+        return ResponseEntityBuilder.success()
+                .message("新增成功")
+                .build();
+    }
+
     @GetMapping("")
-    public ResponseEntity<String> searchAll() {
+    public ResponseEntity<String> searchAll(HttpServletRequest request) {
         ArrayData arrayData = new ArrayData();
         for (GroupBean groupBean : groupService.searchAll()) {
             ObjectData objectData = arrayData.addObject();
@@ -62,7 +83,8 @@ public class GroupController {
     public ResponseEntity<String> searchUsersByGroupId(@PathVariable("groupId") Integer groupId,
                                                        @RequestParam(value = "userName", required = false) String userName,
                                                        @RequestParam(value = "department", required = false) String department,
-                                                       @RequestParam(value = "position", required = false) String position) {
+                                                       @RequestParam(value = "position", required = false) String position,
+                                                       HttpServletRequest request) {
         ArrayData arrayData = new ArrayData();
         for (UserAccountBean userAccountBean : userGroupService
                 .searchAllUserByGroupId(groupId, userName, department, position)) {
@@ -84,7 +106,8 @@ public class GroupController {
     @PatchMapping("/{id}")
     public ResponseEntity<String> patchGroupName(@PathVariable("id") Integer id,
                                                  @RequestBody GroupBean groupBean,
-                                                 BindingResult bindingResult) {
+                                                 BindingResult bindingResult,
+                                                 HttpServletRequest request) {
         BindingResultUtils.validate(bindingResult);
         groupService.update(id, groupBean);
         return ResponseEntityBuilder.success()
@@ -94,15 +117,26 @@ public class GroupController {
 
     @DeleteMapping("/user")
     public ResponseEntity<String> removeUserFromGroup(@RequestParam("userId") String userId,
-                                                      @RequestParam("groupId") Integer groupId) {
+                                                      @RequestParam("groupId") Integer groupId,
+                                                      HttpServletRequest request) {
         userGroupService.removeUserFromGroup(userId, groupId);
         return ResponseEntityBuilder.success()
                 .message("移除成功")
                 .build();
     }
 
+    @DeleteMapping("/chart")
+    public ResponseEntity<String> removeChartFromGroup(@RequestParam("chartId") Integer chartId,
+                                                      @RequestParam("groupId") Integer groupId,
+                                                      HttpServletRequest request) {
+        chartGroupService.removeChartFromGroup(chartId, groupId);
+        return ResponseEntityBuilder.success()
+                .message("移除成功")
+                .build();
+    }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> delGroup(@PathVariable("id") Integer id) {
+    public ResponseEntity<String> delGroup(@PathVariable("id") Integer id, HttpServletRequest request) {
         GroupBean groupBean = new GroupBean();
         groupBean.setAvailable(false);
         groupService.update(id, groupBean);
--- src/main/java/tw/edu/ntub/imd/birc/sodd/service/impl/ChartGroupServiceImpl.java
+++ src/main/java/tw/edu/ntub/imd/birc/sodd/service/impl/ChartGroupServiceImpl.java
@@ -2,9 +2,11 @@ package tw.edu.ntub.imd.birc.sodd.service.impl;
 
 import org.springframework.stereotype.Service;
 import tw.edu.ntub.imd.birc.sodd.bean.ChartGroupBean;
+import tw.edu.ntub.imd.birc.sodd.bean.UserGroupBean;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.dao.ChartGroupDAO;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.ChartGroup;
 import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.ChartGroupId;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.UserGroupId;
 import tw.edu.ntub.imd.birc.sodd.service.ChartGroupService;
 import tw.edu.ntub.imd.birc.sodd.service.transformer.ChartGroupTransformer;
 
@@ -23,5 +25,15 @@ public class ChartGroupServiceImpl extends BaseServiceImpl<ChartGroupBean, Chart
     public ChartGroupBean save(ChartGroupBean chartGroupBean) {
         ChartGroup chartGroup = transformer.transferToEntity(chartGroupBean);
         return transformer.transferToBean(chartGroupDAO.save(chartGroup));
+    }
+
+    @Override
+    public void removeChartFromGroup(Integer chartId, Integer groupId) {
+        ChartGroupBean chartGroupBean = new ChartGroupBean();
+        chartGroupBean.setAvailable(false);
+        ChartGroupId chartGroupId = new ChartGroupId();
+        chartGroupId.setChartId(chartId);
+        chartGroupId.setGroupId(groupId);
+        super.update(chartGroupId, chartGroupBean);
     }
 }
