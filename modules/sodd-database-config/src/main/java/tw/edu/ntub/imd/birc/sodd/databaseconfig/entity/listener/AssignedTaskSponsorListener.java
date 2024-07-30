39eb936f510ef7b14b5d0c9566d37c117746c54b
jerrylin <jerrylin920316@gmail.com>
2024-07-30 17:16:40 +0800

    修改 - 交辦、交辦發起人、匯出Listener

--- modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/entity/listener/AssignedTaskSponsorListener.java
+++ modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/entity/listener/AssignedTaskSponsorListener.java
@@ -1,4 +1,42 @@
 package tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.listener;
 
+import tw.edu.ntub.birc.common.util.StringUtils;
+import tw.edu.ntub.imd.birc.sodd.config.util.SecurityUtils;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.AssignedTaskSponsor;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
 public class AssignedTaskSponsorListener {
+    @PrePersist
+    public void preSave(AssignedTaskSponsor assignedTaskSponsor) {
+        String userId = SecurityUtils.getLoginUserAccount();
+        LocalDateTime now = LocalDateTime.now();
+        if (assignedTaskSponsor.getAvailable() == null) {
+            assignedTaskSponsor.setAvailable(true);
+        }
+        if (StringUtils.isBlank(assignedTaskSponsor.getCreateId())) {
+            assignedTaskSponsor.setCreateId(userId);
+        }
+        if (assignedTaskSponsor.getCreateDate() == null) {
+            assignedTaskSponsor.setCreateDate(now);
+        }
+        if (assignedTaskSponsor.getModifyId() == null) {
+            assignedTaskSponsor.setModifyId(userId);
+        }
+        if (assignedTaskSponsor.getModifyDate() == null) {
+            assignedTaskSponsor.setModifyDate(now);
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(AssignedTaskSponsor assignedTaskSponsor) {
+        if (assignedTaskSponsor.getModifyId() == null) {
+            assignedTaskSponsor.setModifyId(SecurityUtils.getLoginUserAccount());
+        }
+        if (assignedTaskSponsor.getModifyDate() == null) {
+            assignedTaskSponsor.setModifyDate(LocalDateTime.now());
+        }
+    }
 }
--- modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/entity/listener/AssignedTasksListener.java
+++ modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/entity/listener/AssignedTasksListener.java
@@ -1,4 +1,42 @@
 package tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.listener;
 
+import tw.edu.ntub.birc.common.util.StringUtils;
+import tw.edu.ntub.imd.birc.sodd.config.util.SecurityUtils;
+import tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.AssignedTasks;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
 public class AssignedTasksListener {
+    @PrePersist
+    public void preSave(AssignedTasks assignedTask) {
+        String userId = SecurityUtils.getLoginUserAccount();
+        LocalDateTime now = LocalDateTime.now();
+        if (assignedTask.getAvailable() == null) {
+            assignedTask.setAvailable(true);
+        }
+        if (StringUtils.isBlank(assignedTask.getCreateId())) {
+            assignedTask.setCreateId(userId);
+        }
+        if (assignedTask.getCreateDate() == null) {
+            assignedTask.setCreateDate(now);
+        }
+        if (assignedTask.getModifyId() == null) {
+            assignedTask.setModifyId(userId);
+        }
+        if (assignedTask.getModifyDate() == null) {
+            assignedTask.setModifyDate(now);
+        }
+    }
+
+    @PreUpdate
+    public void preUpdate(AssignedTasks assignedTask) {
+        if (assignedTask.getModifyId() == null) {
+            assignedTask.setModifyId(SecurityUtils.getLoginUserAccount());
+        }
+        if (assignedTask.getModifyDate() == null) {
+            assignedTask.setModifyDate(LocalDateTime.now());
+        }
+    }
 }
