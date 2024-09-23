657f7aa354af8aa4b26f3978c519a287bd81fa69
jerrylin <jerrylin920316@gmail.com>
2024-09-23 10:39:43 +0800

    新增 - mysql mssql 雙DB與同步配置

--- modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/Config.java
+++ modules/sodd-database-config/src/main/java/tw/edu/ntub/imd/birc/sodd/databaseconfig/Config.java
@@ -1,24 +1,73 @@
 package tw.edu.ntub.imd.birc.sodd.databaseconfig;
 
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.interceptor.*;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration("databaseConfig")
-@EnableJpaRepositories(basePackages = "tw.edu.ntub.imd.birc.sodd.databaseconfig.dao")
+@EnableJpaRepositories(
+        basePackages = "tw.edu.ntub.imd.birc.sodd.databaseconfig.dao",
+        entityManagerFactoryRef = "mysqlEntityManagerFactory",
+        transactionManagerRef = "mysqlTransactionManager")
 @EnableTransactionManagement
 @EntityScan(basePackages = "tw.edu.ntub.imd.birc.sodd.databaseconfig.entity")
 public class Config {
-    public static final String DATABASE_NAME = "113-smart_opeartion_digital_dashboard";
+    public static final String DATABASE_NAME = "113-smart_operation_digital_dashboard";
+
+
+    @Primary
+    @Bean(name = "mysqlEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean mysqlEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("mysqlDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("tw.edu.ntub.imd.birc.sodd.databaseconfig.entity")
+                .persistenceUnit("mysql")
+                .build();
+    }
+
+    @Primary
+    @Bean(name = "mysqlTransactionManager")
+    public PlatformTransactionManager mysqlTransactionManager(
+            @Qualifier("mysqlEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+
+
+    @Bean(name = "mssqlEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean mssqlEntityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            @Qualifier("mssqlDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("tw.edu.ntub.imd.birc.sodd.databaseconfig.entity.erp")
+                .persistenceUnit("mssql")
+                .build();
+    }
+
+    @Bean(name = "mssqlTransactionManager")
+    public PlatformTransactionManager mssqlTransactionManager(
+            @Qualifier("mssqlEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
 
     @Bean
     public TransactionInterceptor transactionInterceptor(TransactionManager transactionManager) {
@@ -46,3 +95,4 @@ public class Config {
         return new TransactionInterceptor(transactionManager, attributeSource);
     }
 }
+
