ed172715a195520a70c4366d3a0dc59e46796a01
simon11046041 <11046041@ntub.edu.tw>
2024-10-21 19:31:04 +0800

    修改 - 產量達成率圖表

--- UploadFile/file/script/產量達成率/yield_achievement_rate.py
+++ UploadFile/file/script/產量達成率/yield_achievement_rate.py
@@ -13,6 +13,9 @@ def generate_html_chart(file_name):
     # 將日期轉換為日期格式
     df['date'] = pd.to_datetime(df['date'])
 
+    # 篩選最近七天的數據
+    df = df[df['date'] >= pd.to_datetime('today') - pd.Timedelta(days=7)]
+
     # 將數據轉換為數字型
     product_numbers = df['productNumber']
 
