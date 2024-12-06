722bd79182478860aeaf5435ca22fc282414f896
simon11046041 <11046041@ntub.edu.tw>
2024-12-06 20:55:46 +0800

    修改 - 工時效率圖表

--- UploadFile/file/script/工時效率/time_efficiency.py
+++ UploadFile/file/script/工時效率/time_efficiency.py
@@ -3,6 +3,7 @@ import io
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.io as pio
+import sys
 
 
 def generate_unstacked_area_chart(file_name):
@@ -22,15 +23,15 @@ def generate_unstacked_area_chart(file_name):
     # 確保日期欄位轉換為日期格式
     df['date'] = pd.to_datetime(df['date'])
 
-    # 找出所有數值欄位（假設非日期欄位均為數據）
-    value_columns = [col for col in df.columns if col != 'date']
+    # 篩選數值型欄位
+    numeric_columns = df.select_dtypes(include=['number']).columns.tolist()
 
     # 創建未堆疊面積圖
     fig = go.Figure()
 
     # 動態添加每一條數據線及其面積
     colors = ['blue', 'red', 'green', 'orange', 'purple', 'brown', 'pink', 'gray', 'olive', 'cyan']
-    for i, col in enumerate(value_columns):
+    for i, col in enumerate(numeric_columns):
         fig.add_trace(go.Scatter(
             x=df['date'],
             y=df[col],
@@ -48,20 +49,10 @@ def generate_unstacked_area_chart(file_name):
         xaxis=dict(autorange=True),
         yaxis=dict(autorange=True),
         autosize=True,
-        legend_title="產線名稱",    # 顯示圖表旁邊的產線名稱標籤
-        showlegend=True  # 確保顯示圖例
+        legend_title="產線名稱",
+        showlegend=True
     )
 
     # 儲存圖表為互動式 HTML
     pio.write_html(fig, file_name)
 
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 3:
-        print(f"用法: python script.py html [output_file_name.html]")
-    elif sys.argv[1].lower() == 'html':
-        generate_unstacked_area_chart(sys.argv[2])
-    else:
-        print("無效的參數，請使用 'html'")
